17afe1e80ffd1d754d31c4c272f04d8e8ab36d21
abhinav.goel <abhinav.goel@tingcore.com>
2018-02-19 12:13:53 +0100

    react slideshow

--- lifeinmm-web/src/components/gallery/Gallery.js
+++ lifeinmm-web/src/components/gallery/Gallery.js
@@ -2,14 +2,31 @@ import React from 'react'
 import Slideshow from '../slideshow';
 
 const items = [
-    './images/1.jpg',
-    './images/2.jpg',
-    './images/3.jpg',
-    './images/4.jpg',
+    '/images/1.jpg',
+    '/images/2.jpg',
+    '/images/3.jpg',
+    '/images/4.jpg',
 ];
 
 export default class Gallery extends React.Component {
     render() {
+        // var finder = require('findit')(process.argv[2] || '.');
+        // var path = require('path');
+        //
+        // finder.on('directory', function (dir, stat, stop) {
+        //     var base = path.basename(dir);
+        //     if (base === '.git' || base === 'node_modules') stop()
+        //     else console.log(dir + '/')
+        // });
+        //
+        // finder.on('file', function (file, stat) {
+        //     console.log(file);
+        // });
+        //
+        // finder.on('link', function (link, stat) {
+        //     console.log(link);
+        // });
+
         return <Slideshow
             showIndex={false}
             showArrows={false}
